5c3b318b97e77c571de898cc1a10b2f45c53320c
Elias <eliasrolonsoto@gmail.com>
2023-05-18 16:38:42 -0300

    Add project files.

---
+++ Movies.Data/MovieRepository.cs
@@ -0,0 +1,26 @@
+﻿using Movies.Entities;
+
+namespace Movies.Data
+{
+    public class MovieRepository
+    {
+        public List<Movie> GetAll()
+        {
+            var movies = new List<Movie>();
+
+
+            //Generador artificial (supolencia de DB)
+            //for(int i =1 ; i <= 15; i++)
+            //{
+            //    movies.Add(new Movie()
+            //    {
+            //        MovieId = i,
+            //        Name = $"Movie #{i}",
+            //        ImageUrl = string.Empty
+            //    });
+            //}
+
+            return movies;
+        }
+    }
+}
